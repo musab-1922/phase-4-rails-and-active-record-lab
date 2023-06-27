d3687b51c19f2f7003ce97fc15580b01f325d478
musab <musababdi1922@gmail.com>
2023-06-27 14:53:46 +0300

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+student = Student.new(first_name: "Dwayne", last_name: "Johnson")
+puts student.to_s # Output: "Dwayne Johnson"
